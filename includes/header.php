4fca49343f0a4496dd2cd56ba5772447cc34e272
arspineda2 <arspineda8@gmail.com>
2026-02-05 12:46:57 +0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Community Portal</title>
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+
+    <nav class="navbar">
+        <div class="logo">Community Portal</div>
+        <div class="nav-links">
+            <a href="#" class="nav-item active"><i class="fa-solid fa-house"></i> Home</a>
+            <a href="about.php" class="nav-item"><i class="fa-solid fa-circle-info"></i> About</a>
+            <a href="trashsched.php" class="nav-item"><i class="fa-solid fa-calendar-days"></i> Trash Schedule</a>
+            <a href="reports.php" class="nav-item"><i class="fa-solid fa-comment-dots"></i> Reports</a>
+            <a href="contact.php" class="nav-item"><i class="fa-solid fa-phone"></i> Contact</a>
+        </div>
+        <div class="user-actions">
+            <button class="btn-user"><i class="fa-solid fa-user"></i> User</button> <!-- Adjustment kailangan mag-change pag ka Admin ang naka-login -->
+            <button class="btn-logout"><i class="fa-solid fa-right-from-bracket"></i> Logout</button>
+        </div>
+</nav>
+</body>
+</html>
