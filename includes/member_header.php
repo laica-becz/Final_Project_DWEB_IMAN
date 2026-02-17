adf5b62afa376e35ff901bda7fb72bb38542487e
laica-becz <zepedabiancaellise@gmail.com>
2026-02-17 13:24:45 +0800

    Update and rename header.php to member_header.php

--- includes/member_header.php
+++ includes/member_header.php
@@ -5,24 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Community Portal</title>
     <link rel="stylesheet" href="header.css">
-    <link rel="stylesheet" href="reports.css">
+   <!--- <link rel="stylesheet" href="reports.css"> --->
 </head>
 <body>
 
     <nav class="navbar">
         <div class="logo">Community Portal</div>
         <div class="nav-links">
-            <a href="#" class="nav-item active"><i class="fa-solid fa-house"></i> Home</a>
+            <a href="member_portal.php" class="nav-item active"><i class="fa-solid fa-house"></i> Home</a>
             <a href="about.php" class="nav-item"><i class="fa-solid fa-circle-info"></i> About</a>
-            <a href="trashsched.php" class="nav-item"><i class="fa-solid fa-calendar-days"></i> Trash Schedule</a>
-            <a href="reports.php" class="nav-item"><i class="fa-solid fa-comment-dots"></i> Reports</a>
+            <a href="member_trashSched.php" class="nav-item"><i class="fa-solid fa-calendar-days"></i> Trash Schedule</a>
+            <a href="member_reports.php" class="nav-item"><i class="fa-solid fa-comment-dots"></i> Reports</a>
             <a href="contact.php" class="nav-item"><i class="fa-solid fa-phone"></i> Contact</a>
         </div>
         <div class="user-actions">
-            <button class="btn-user"><i class="fa-solid fa-user"></i> User</button> <!-- Adjustment kailangan mag-change pag ka Admin ang naka-login -->
+            <button class="btn-user"><i class="fa-solid fa-user"></i> User</button>
             <button class="btn-logout"><i class="fa-solid fa-right-from-bracket"></i> Logout</button>
         </div>
 </nav>
 </body>
-
 </html>
