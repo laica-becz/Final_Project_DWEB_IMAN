78e47a3340baa086e240465dbe2ba14f6c54ce91
laica-becz <zepedabiancaellise@gmail.com>
2026-02-17 13:55:29 +0800

    Add about.php for community portal information

---
+++ member/about.php
@@ -0,0 +1,16 @@
+<?php require_once '../includes/member_header.php'; ?>
+<?php require_once '../includes/about_content.php'; ?>
+<?php require_once '../includes/member_footer.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Community Portal</title>
+    <link rel="stylesheet" href="member/about.css">
+</head>
+<body>
+    
+</body>
+</html>
