4fc212ebe44ba969d3f6e95cc11871c1386f6268
Justine Enriquez <enriquezjustinepaul@gmail.com>
2026-02-17 20:40:31 +0800

    Update trashsched.php

--- trashsched.php
+++ trashsched.php
@@ -40,7 +40,7 @@ $guidelines = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trash Collection Schedule</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="trashsched.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
@@ -105,4 +105,5 @@ $guidelines = [
 
 </body>
 </html>
+
 
