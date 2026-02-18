f9e32d3133e0697d4258853eac92f1be11e89424
Justine Enriquez <enriquezjustinepaul@gmail.com>
2026-02-18 17:37:35 +0800

    Rename trashsched.php to member_trashschedule.php

--- member/member_trashschedule.php
+++ member/member_trashschedule.php
@@ -40,7 +40,7 @@ $guidelines = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trash Collection Schedule</title>
-    <link rel="stylesheet" href="../member/trashsched.css">
+    <link rel="stylesheet" href="member/member_trashschedule.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
@@ -105,3 +105,4 @@ $guidelines = [
 
 </body>
 </html>
+
