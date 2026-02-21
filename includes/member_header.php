e5a9888bbddf57ce3b52bef4d1f05307b39eff6c
laica-becz <zepedabiancaellise@gmail.com>
2026-02-21 17:08:51 +0800

    Update stylesheet path in member_header.php

--- includes/member_header.php
+++ includes/member_header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../includes/header.css">
+    <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
@@ -26,3 +26,4 @@
 </nav>
 </body>
 </html>
+
