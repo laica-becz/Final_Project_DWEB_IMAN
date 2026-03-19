5f12786395b74606b83417e6870e37c20807676b
laica-becz <zepedabiancaellise@gmail.com>
2026-03-19 22:29:39 +0800

    Update about.php

--- admin/about.php
+++ admin/about.php
@@ -1,3 +1,4 @@
+<?php require_once '../includes/auth_check.php'; ?>
 <?php require_once '../includes/admin_header.php'; ?>
 <?php require_once '../includes/about_content.php'; ?>
 <?php require_once '../includes/footer.php'; ?>
