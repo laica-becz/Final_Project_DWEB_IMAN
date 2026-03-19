b626417baa88f77345c52f596bf70200f626e770
laica-becz <zepedabiancaellise@gmail.com>
2026-03-19 22:31:54 +0800

    Update contact.php

--- admin/contact.php
+++ admin/contact.php
@@ -1,4 +1,4 @@
-<?php include "../includes/admin_header.php"; ?>
 <?php require_once '../includes/auth_check.php'; ?>
+<?php include "../includes/admin_header.php"; ?>
 <?php include "../includes/contact_content.php"; ?>
 <?php include "../includes/footer.php"; ?>
