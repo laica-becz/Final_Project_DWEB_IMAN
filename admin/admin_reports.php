60903dd3d330dfb997565caa9379d9794c59525d
laica-becz <zepedabiancaellise@gmail.com>
2026-03-19 19:01:23 +0800

    Update admin_reports.php

--- admin/admin_reports.php
+++ admin/admin_reports.php
@@ -1,6 +1,7 @@
 <?php 
 include "../includes/admin_header.php"; 
 include "../includes/db_conn.php";
+require_once '../includes/auth_check.php';
 
 //HANDLE ADMIN ANNOUNCEMENT (ADD NEW NOTICE BUTTON)
 if (isset($_POST['btn_save'])) {
