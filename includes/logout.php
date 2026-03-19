7611ffbe1a0c6268ac76fff8619fea93b611acb7
laica-becz <zepedabiancaellise@gmail.com>
2026-03-20 00:10:57 +0800

    Update logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -1,4 +1,17 @@
 <?php
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'domain' => 'localhost',
+    'secure' => false,
+    'httponly' => true,
+]);
+session_start();
+session_unset();
+session_destroy();
+header('Location: /FinalProject/index.php');
+exit();
+?><?php
 session_start();
 session_destroy();
 header("Location: ../index.php");
