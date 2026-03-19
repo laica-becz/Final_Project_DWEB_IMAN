559a2931eb65635b89de911365aa85f7f3d50146
laica-becz <zepedabiancaellise@gmail.com>
2026-03-19 18:43:23 +0800

    Update member_home.php

--- member/member_home.php
+++ member/member_home.php
@@ -39,6 +39,7 @@ $announcements = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Community Portal</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="../css/member_home.css">
 </head>
 <body>
@@ -69,7 +70,7 @@ $announcements = [
         
     </main>
 
-    <?php include "../includes/member_footer.php"; ?>
+    <?php include "../includes/footer.php"; ?>
 </body>
 </html>
 
