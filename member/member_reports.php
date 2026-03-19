601bd1fe6aa65dc72b5d09c47014662a29209431
laica-becz <zepedabiancaellise@gmail.com>
2026-03-19 18:45:19 +0800

    Update member_reports.php

--- member/member_reports.php
+++ member/member_reports.php
@@ -30,6 +30,7 @@ if (isset($_POST['btn_save']))
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Reports & Concerns - Community Portal</title>
         <link rel="stylesheet" href="../css/member_reports.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     </head>
     <body>
         
