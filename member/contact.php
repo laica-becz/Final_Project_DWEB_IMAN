aa9e41adabd90677cb9722b9ce60669a1ba12c61
laica-becz <zepedabiancaellise@gmail.com>
2026-03-19 18:42:55 +0800

    Update contact.php

--- member/contact.php
+++ member/contact.php
@@ -1,7 +1,3 @@
 <?php include "../includes/member_header.php"; ?>
 <?php include "../includes/contact_content.php"; ?>
-<?php include "../includes/member_footer.php"; ?>
-
-<head>
-    <title>Contact Us - Community Portal</title>
-</head>
+<?php include "../includes/footer.php"; ?>
